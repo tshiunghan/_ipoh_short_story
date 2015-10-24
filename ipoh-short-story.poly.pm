d3afe414e7f607271947fc7eefec7877973cfbde
tshiunghan <tshiunghan@gmail.com>
2015-10-24 23:26:38 +0800

    Final draft
    Final draft of short story.

--- ipoh-short-story.poly.pm
+++ ipoh-short-story.poly.pm
@@ -2,13 +2,138 @@
 
 ◊heading{Ipoh Short Story}
 
-Once upon a time in Ipoh, a guy from KL came up for the weekend
-and wrote a short story for a festival. The ◊emph{weekend}.
+I was driving around the parking lot, looking for a place to park. But there were no parking spaces. I swore at myself for entering the parking lot on Basement 1. I should have entered on Basement 2. The line of cars in front of me slowly worked its way down to the lower levels. The line of cars behind me extended further than I could see. The queue would stop for a long time, every once in a while. I would stretch my neck to look for the source of the obstruction, but it would never be visible. 
 
-He really wanted to join this writing workshop.
+Every once in a while, someone would walk by my car and I would get excited. Maybe that person had parked in a lot adjacent to me. But the person would walk perpendicular to me, into the next row of cars. I would swore at myself when this happened. 
 
-The date was ◊(get-date).
+I dialed Andrew, to tell him I would be late. He didn't pick up, and the call went to his voicemail. I hung up and wrote him a text.
 
-He really wanted to go, but he didn't want to spend any money.
+There was parking on Basement 2. I parked near the escalator. I took off my driving shoes and put on my walking shoes. 
 
-He was broke. He had lost his job. His girlfriend had left him.
+Getting out of my car, I could hear a cat meowing. I was meowing a lot. Maybe it wants water or food. Maybe it's scared. I looked for it, but I was late for lunch. Anyways, if I found it, there's nothing I could do for it. I didn't have any cat food in my car. I didn't have any water. And I probably couldn't bring a cat into the shopping mall. Could I? I seriously considered it for a few seconds, but I ruled that it would be a bad idea. 
+
+I swore at myself and headed into the shopping mall.
+
+Andrew was leaning against a wall near Coffee Bean. He was vaping, but as I approached him someone asked him to stop. He made a violent gesture. The person walked away, looking at him as he did so. 
+
+I waved at him. He waved back.
+
+"What was that all about?" I said.
+
+"Nothing," he said, "You hungry?"
+
+I nodded.
+
+Nando's was semi-crowded, but we managed to get a booth. I ordered lemon and herb. He ordered extra hot. 
+
+"My umbrella broke yesterday," he said.
+
+"Yah," I said, "I see the way you treat it."
+
+"There's always a broken umbrella in someone's car," he said. 
+
+I didn't say anything.
+
+"What time did you get here?" I said, changing the subject.
+
+"I left the office early," he said, taking out his phone.
+
+I took out my phone as well and opened Facebook.
+
+My cousin got married a few weeks ago, but she had gone through the photos yesterday. The photo at the top of the feed was her kissing her husband, who looked surprised. Her face was in profile and her husband's was facing the camera.
+
+I rolled my eyes and switched to Gmail. Client had emailed about latest draft of the annual report. It was a long email. I skimmed it and decided to wait until I was back in the office. Then I put my phone to sleep.
+
+Andrew was looking at me.
+
+"Annie, I need a sign from you," he said.
+
+I was quiet.
+
+"Something," he said.
+
+I lowered my eyes, I couldn't help it.
+
+"I need you to get serious."
+
+I sighed. 
+
+"You watch too much Youtube. Why aren't you doing something more productive? Are you sick? Are you depressed? Do you know what you want? Put down your glass and tell me what you want in life."
+
+He paused. 
+
+"Tell me where you want to be in five years."
+
+I raised me eyes to meet his.
+
+"I can't," I said.
+
+"You can't?" he said.
+
+"Please don't ask me again," I said.
+
+"I... look..." he began.
+
+"Do you know where you want to be in five years?" I said.
+
+"I do," he said.
+
+"Where?" I said.
+
+"I want to be with you. I want to be married to you, have a kid, maybe? Do you want to have a kid? I think we would be good parents. I think we could raise a kid really well."
+
+"You don't know until you do it," I said.
+
+"Yeah, I know," he said, "but I want to do it with you."
+
+"God," I said, rolling my eyes.
+
+I took a sip of my tea. It was tepid. 
+
+Andrew asked for the check.
+
+He leaned forward in his seat, looking at me, as if he was going to say something. But he didn't want to say anything, he was waiting for an answer. 
+
+"I need more time, Andrew," I said. I put my hand over his hand and held it.
+
+"I'm scared, baby," he said.
+
+"I know, baby," I said. 
+
+"Can you give me a bit more?" he said.
+
+"I'm sorry," I said.
+
+"I don't know, baby," he said. It was his turn to lower his eyes.
+
+"Listen, baby," I said. 
+
+He looked at me.
+
+I said, "I used to know what it means to know. I know two plus two equals four. I didn't know and now I know."
+
+"Ok," he said.
+
+"But knowing became harder, less certain," I continued, "I thought I knew more than I did, but I don't. I know nothing, less than nothing. Less than one percent of everything there is to know."
+
+He was quiet.
+
+"Two plus two does not always equal four. It equals one or two or three sometimes. I think at one time I had to choose what it means and I made the same choice everyone else makes.
+
+"Baby..." he said.
+
+"But I can choose not to choose," I said, "Or I can choose one answer on one day and another answer on another day. I can choose two answers, I can choose four. I can't do it with all choices, but I can do it with this one."
+
+I paused.
+
+"Do you know what I mean?"
+
+He was looking at me as if he didn't know me. 
+
+"It's ok," I said, "it's ok."
+
+The check arrived.
+
+We walked into the temperature controlled mall, holding hands. He asked me if he could walk me to my car, I said I'm ok. He asked me if we could have dinner, and I said yeah. Then we were no longer holding hands, I was walking to my car. I was driving out of the parking lot and back to the office. That's when I noticed the light.
+
+There is only everything, lit with amazing light.
