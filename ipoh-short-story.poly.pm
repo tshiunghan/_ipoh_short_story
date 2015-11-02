aebc15f4f40a9d2eb2827a78db5e09aa00088f2d
tshiunghan <tshiunghan@gmail.com>
2015-11-02 15:58:47 +0800

    Merging with master
    Merging with master for the first time!

--- ipoh-short-story.poly.pm
+++ ipoh-short-story.poly.pm
@@ -1,14 +1,149 @@
 #lang pollen
 
-◊heading{Ipoh Short Story}
+◊heading{Ipoh Parade}
 
-Once upon a time in Ipoh, a guy from KL came up for the weekend
-and wrote a short story for a festival. The ◊emph{weekend}.
+There was no place to park. I swore at myself for leaving the office late. The line of cars in front of me slowly worked its way down to the lower levels of the parking lot. The line of cars behind me extended further than I could see. 
 
-He really wanted to join this writing workshop.
+The queue would stop for a long time, every once in a while. I would stretch my neck to look for the source of the obstruction, but I would never see it. 
 
-The date was ◊(get-date).
+Someone would walk by my car and I would get excited. I would think, maybe that person had parked near me, and I could take his spot. But the person would walk into the next row of cars. He had walked slow enough to get my hopes up. I would swear at myself when this happened. 
 
-He really wanted to go, but he didn't want to spend any money.
+I dialed Andrew, to tell him I would be late. He didn't pick up, and the call went to voicemail. I hung up and texted him.
 
-He was broke. He had lost his job. His girlfriend had left him.
+There was ample parking on Basement 2. What was all the fuss about? I aimed for a parking space near the escalator, then I changed my mind and parked near the staircase, then I changed my mind and parked beside the escalator. Another car didn't like my indecisiveness and horned me.
+
+After parking, I checked my makeup in the rearview mirror. It was an empty gesture, I wasn't going to bother re-applying it. It was fine, anyway. I took off my driving shoes. I touched my office heels for a second, but decided to leave them in the car, and put on my walking shoes.
+
+Walking to the escalator, I could hear a cat meowing. It was meowing a lot. Maybe it wants water or food, I thought. Maybe it's scared. I looked for it, but I was late. Anyways, there's nothing I can do for it. I didn't have any cat food in my car. I didn't have any water. And I couldn't bring a cat into the shopping mall. Could I? I weighed the decision carefully for a few seconds, but decided that it would be a bad idea. 
+
+I swore at myself and walked into the shopping mall.
+
+Andrew was wearing the dress shirt I had bought him for his birthday. Uh-oh, I thought. He was leaning against a wall near Coffee Bean, vaping. As I approached him someone walked up to him and said something to him. He made a violent gesture. The person walked away, looking at Andrew as he did so. 
+
+Andrew saw me. I waved at him. He waved back.
+
+"What was that all about?" I said.
+
+"Nothing," he growled, "you hungry?"
+
+I nodded.
+
+The chicken place was semi-crowded, but we managed to get a booth. I ordered lemon and herb. He ordered extra hot. 
+
+"My umbrella broke yesterday," he said.
+
+"Yah," I said, "I see the way you treat it."
+
+"There's always a broken umbrella in someone's car," he said. 
+
+I didn't say anything.
+
+"What time did you get here?" I said, changing the subject.
+
+"I left the office early," he said, taking out his phone.
+
+I took out my phone and opened Facebook.
+
+My cousin got married a few weeks ago, and she had gone through the photos yesterday. At the top of my feed, a photo of her kissing her husband on the cheek. Her face was in profile and her husband's was facing the camera. Her husband's face said, "Oh no!"
+
+I rolled my eyes and switched to Gmail. Client had emailed about latest draft of annual report. It was a long email. I skimmed it for anything demanding immediate attention and, finding nothing, decided to wait until I was back in the office to reply. Then I put my phone to sleep.
+
+Andrew was looking at me. I don't know how long he had been looking at me.
+
+"Annie," he said.
+
+Here we go, I thought.
+
+"I need a sign from you."
+
+I lowered my eyes, I couldn't help it.
+
+"I need you to get serious."
+
+I sighed. 
+
+He continued, "You watch too much Youtube. Why aren't you doing something more productive? Are you sick? Are you depressed? Do you know what you want? Put down your glass and tell me what you want in life."
+
+I put my glass down. He paused. 
+
+"Tell me where you want to be in five years."
+
+I raised my eyes to meet his.
+
+"I can't," I said.
+
+"You can't?"
+
+"Don't ask me again," I said, growling.
+
+"I... look...."
+
+We looked at each other for a moment. The food arrived and we began to eat. I separated the meat from the bone as he talked, setting aside the cartilege, which I gave to Andrew. It had become a habit.
+
+"Do you know where you want to be in five years?" I said.
+
+"I do," he said.
+
+"Where?"
+
+"I want to be with you. I want to be married to you. Have a kid, maybe? Do you want to have a kid? I think we would be good parents. I think we could raise a kid really well."
+
+"You don't know until you do it."
+
+"Yeah," he said, "but I want to do it with you."
+
+"God." I rolled my eyes.
+
+I took a sip of my iced tea. It was indeed iced.
+
+Andrew asked for the check.
+
+He leaned forward in his seat, looking at me, as if he was going to say something. But he wasn't going to say anything, he wanted me to say something. He was waiting for an answer. 
+
+"I need more time, Andrew," I said. I put my hand over his hand and held him. He was warm, too warm. Maybe he was tired. Maybe he wasn't getting enough sleep. Maybe it was the chicken.
+
+"I'm scared, baby."
+
+"I know, baby."
+
+"Can you give me a bit more?"
+
+"I'm sorry."
+
+"I don't know, I don't know." It was his turn to lower his eyes.
+
+We sat like that for a minute, because what the hell could I do? It was kind of nice, though. I wish I could have lived inside the carcass of that moment forever.
+
+"Look at me," I said. 
+
+He looked at me.
+
+I said, "I used to know what it means to know. I know two plus two equals four. I didn't know and then I knew."
+
+"Ok," he said.
+
+"But knowing became harder, less certain," I continued, "I thought I knew more than I did, but I know nothing. Less than nothing. Less than one percent of one percent of everything there is to know."
+
+He was quiet.
+
+"Two plus two does not always equal four. It equals one or two or three, sometimes. I think one day I had to choose what it means and I made the same choice everyone else makes."
+
+Andrew flipped his hand over and held my hand in his.
+
+"But I can choose not to choose," I said. "Or I can choose one answer on one day and another answer on another day. I can choose two answers, I can choose four."
+
+I paused.
+
+"I can't do it with all choices, but I can do it with this one."
+
+He was looking at me as if he didn't know what I mean. 
+
+"Do you know what I mean?"
+
+The check arrived.
+
+"It's ok," I said, "it's ok."
+
+We walked into the temperature-controlled mall, holding hands. He asked me if he could walk me to my car. I said I'm ok, I have to rush. He asked me if we could have dinner, and I said yeah. Then we were no longer holding hands, I was walking to my car. I was driving out of the parking lot and back to the office. I was looking out of the window, into the day. And I noticed the light.
+
+There is only everything, lit with amazing light.
